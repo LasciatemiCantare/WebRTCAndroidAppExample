078c5990a609230e832461c98ab2e855d14cba6a
Sergey Shipilov <sergey.shipilov1999@gmail.com>
2024-01-09 05:37:37 +0300

    Replaced WebRTC library and updated other libraries

--- app/src/main/kotlin/com/android/webrtc/example/application/WebRTCExampleApplication.kt
+++ app/src/main/kotlin/com/android/webrtc/example/application/WebRTCExampleApplication.kt
@@ -5,11 +5,6 @@ import android.content.Context
 import com.android.webrtc.example.ioc.ServiceLocator
 
 class WebRTCExampleApplication : Application() {
-    init {
-        // loading native WebRTC library on app start
-        System.loadLibrary("jingle_peerconnection_so")
-    }
-
     override fun attachBaseContext(base: Context) {
         super.attachBaseContext(base)
         ServiceLocator.initWithContext(base)
